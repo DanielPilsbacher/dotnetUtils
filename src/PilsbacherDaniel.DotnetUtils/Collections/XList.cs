212cb29696f239b367fbcd6b51f8ee737b9b6941
Daniel Pilsbacher <126380398+DanielPilsbacher@users.noreply.github.com>
2025-11-27 09:55:00 +0100

    feat: add custom list to easy add new item-values
    - introduce XList<T> derived by List<T> to add easily new item with bracket index [int]

---
+++ src/PilsbacherDaniel.DotnetUtils/Collections/XList.cs
@@ -0,0 +1,29 @@
+﻿namespace PilsbacherDaniel.DotnetUtils.Collections;
+
+public class XList<T> : List<T>
+{
+    public new T this[int index]
+    {
+        get
+        {
+            return base[index];
+        }
+
+        set
+        {
+            if (index == Count)
+            {
+                Add(value);
+            }
+            if (index >= Count)
+            {
+                int count = Count;    // counter migth change by adding values
+                for (int i = 0; i <= index - count; i++)
+                {
+                    Add(default!);
+                }
+            }
+            base[index] = value;
+        }
+    }
+}
